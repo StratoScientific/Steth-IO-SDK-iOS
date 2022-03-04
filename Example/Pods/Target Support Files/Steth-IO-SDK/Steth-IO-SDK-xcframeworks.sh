32d4a8f2910f988afc506d77896a0aae4b2a519d
Naveen Paulsingh <naveen.p@ionixxtech.com>
2022-03-04 21:32:44 +0530

    Release 0.1.4.

--- Example/Pods/Target Support Files/Steth-IO-SDK/Steth-IO-SDK-xcframeworks.sh
+++ Example/Pods/Target Support Files/Steth-IO-SDK/Steth-IO-SDK-xcframeworks.sh
@@ -105,5 +105,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/../../Steth-IO-SDK/Frameworks/StethIO.xcframework" "Steth-IO-SDK" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/../../Steth-IO-SDK/Frameworks/StethIO.xcframework" "Steth-IO-SDK" "framework" "ios-arm64_x86_64-simulator" "ios-arm64"
 
